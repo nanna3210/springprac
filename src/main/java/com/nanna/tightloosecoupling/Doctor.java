d2705a31ad8310d2b0ae04e16fdb70831bddc87d
nanna <navkishoredas@gmail.com>
2022-12-21 22:54:32 +0530

    tigght loose coupling

--- src/main/java/com/nanna/tightloosecoupling/Doctor.java
+++ src/main/java/com/nanna/tightloosecoupling/Doctor.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 //@Component
 public class Doctor implements Job{
     
-    
+//    example of loose coupling k
     @Override
     public void displayJob ( ) {
         System.out.println ("i m a doctor");
